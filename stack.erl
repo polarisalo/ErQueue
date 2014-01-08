1e53e7140683a15db313cb87e74e9078c4766103
polarisalo <polarisalo@gmail.com>
2014-01-09 01:45:55 +0800

    finish
    finish stack in erlang

--- stack.erl
+++ stack.erl
@@ -12,32 +12,67 @@
 		 pop/1,
 		 empty/1]).
 -define(MAX_SIZE,max_size).
+-define(EMPTY_STACK,0).
 
 new(StackName) ->
-	set_max_size(StackName,0).
+	set_max_size(StackName,?EMPTY_STACK),
+	ok.
 
 delete(StackName) ->
-	set_max_size(StackName,0).
+	MaxSize = get_max_size(StackName),
+	[erlang:erase({StackName,Local})||Local<-lists:seq(1,MaxSize)],
+	set_max_size(StackName,?EMPTY_STACK),
+	ok.
 
 top(StackName) ->
+	MaxSize = get_max_size(StackName),
+	case MaxSize of
+		?EMPTY_STACK ->
+			{error,empty_stack};
+		_ ->
+			get_data(StackName,MaxSize)
+	end.
 
 push(StackName,Value) ->
 	MaxSize = get_max_size(StackName),
+	NewMaxSize = MaxSize + 1,
+	set_data(StackName,NewMaxSize,Value),
+	set_max_size(StackName,NewMaxSize),
+	Value.
 
+pop(StackName) ->
+	MaxSize = get_max_size(StackName),
+	case MaxSize of
+		?EMPTY_STACK ->
+			{error,empty_stack};
+		_ ->
+			NewMaxSize = MaxSize - 1,
+			set_max_size(StackName,NewMaxSize),
+			get_data(StackName,MaxSize)
+	end.
+
+empty(StackName) ->
+	case get_max_size(StackName) of
+		?EMPTY_STACK ->
+			true;
+		_ ->
+			false
+	end.
+
+set_max_size(StackName,MaxSize) ->
+	erlang:put({StackName,?MAX_SIZE},MaxSize).
 
 get_max_size(StackName) ->
 	MaxSize = erlang:get({StackName,?MAX_SIZE}),
 	case erlang:is_integer(MaxSize) of
 		false ->
-			0;
+			?EMPTY_STACK;
 		true ->
 			MaxSize
 	end.
-set_max_size(StackName,MaxSize) ->
-	erlang:put({StackName,?MAX_SIZE},MaxSize).
 
 set_data(StackName,Local,Value) ->
 	erlang:put({StackName,Local},Value).
-	
+
 get_data(StackName,Local) ->
 	erlang:get({StackName,Local}).
